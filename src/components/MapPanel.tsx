ea3c4408f6437f12192aac03ad97f9e9c1faebbd
John Qian <johnlongqian@gmail.com>
2021-01-26 18:57:14 -0700

    no more relying on re-renders

--- src/components/MapPanel.tsx
+++ src/components/MapPanel.tsx
@@ -18,23 +18,17 @@ const MapPanel: React.FC<MapPanelProps> = ({
   minimized,
 }) => {
   const wrapperRef = React.useRef<HTMLDivElement>(null);
-
-  const [roomState, setRoomState] = React.useState<any>(colyseusRoom.state);
-  const [ticks, setTicks] = React.useState(0);
-
-  console.log('state changed', roomState);
-
-  const {
-    width = window.innerWidth,
-    height = window.innerWidth,
-  } = useResizeObserver({
-    ref: wrapperRef,
+  const windowSize = React.useRef<{ width: number; height: number }>({
+    width: window.innerWidth,
+    height: window.innerHeight,
   });
+
+  const scaleRef = React.useRef(1);
 
   const pixiApp = React.useMemo(() => {
     const app = new PIXI.Application({
-      width,
-      height,
+      width: windowSize.current.width,
+      height: windowSize.current.height,
       antialias: true,
       resolution: window.devicePixelRatio,
       autoDensity: true,
@@ -57,45 +51,40 @@ const MapPanel: React.FC<MapPanelProps> = ({
     return app;
   }, []);
 
-  const playerGraphics: {
-    [sessionId: string]: PIXI.Graphics;
-  } = React.useMemo(() => ({}), []);
-
-  const scale = React.useMemo(() => {
-    return minimized ? 0.5 : 1;
+  React.useEffect(() => {
+    scaleRef.current = minimized ? 0.5 : 1;
   }, [minimized]);
 
   const centerCamera = React.useCallback(() => {
     const player = colyseusRoom.state.players.get(colyseusRoom.sessionId);
 
     if (player != null) {
-      pixiApp.stage.scale.x = scale;
-      pixiApp.stage.scale.y = scale;
-      pixiApp.stage.position.x = -player.x * scale + width / 2;
-      pixiApp.stage.position.y = -player.y * scale + height / 2;
+      pixiApp.stage.scale.x = scaleRef.current;
+      pixiApp.stage.scale.y = scaleRef.current;
+      pixiApp.stage.position.x =
+        -player.x * scaleRef.current + windowSize.current.width / 2;
+      pixiApp.stage.position.y =
+        -player.y * scaleRef.current + windowSize.current.height / 2;
     }
-  }, [colyseusRoom, pixiApp, width, height, scale]);
+  }, [colyseusRoom, pixiApp]);
+
+  useResizeObserver({
+    ref: wrapperRef,
+    onResize(size) {
+      if (size.width != null && size.height != null) {
+        windowSize.current = { width: size.width, height: size.height };
+        centerCamera();
+        pixiApp.renderer.resize(size.width, size.height);
+      }
+    },
+  });
 
   React.useEffect(() => {
-    pixiApp.renderer.resize(width, height);
-    centerCamera();
-  }, [width, height]);
+    const playerGraphics: {
+      [sessionId: string]: PIXI.Graphics;
+    } = {};
 
-  React.useEffect(() => {
-    console.log('attaching global listener...');
-    colyseusRoom.onStateChange((s) => {
-      setRoomState(s);
-      setTicks((t) => t + 1);
-    });
-    return () => {
-      colyseusRoom.removeAllListeners();
-    };
-  }, [colyseusRoom]);
-
-  const { players } = roomState;
-
-  React.useEffect(() => {
-    players.onAdd = (player: any, sessionId: any) => {
+    colyseusRoom.state.players.onAdd = (player: any, sessionId: any) => {
       console.log('Colyseus player added', player);
 
       const graphic = new PIXI.Graphics();
@@ -108,31 +97,25 @@ const MapPanel: React.FC<MapPanelProps> = ({
       graphic.y = player.y;
 
       playerGraphics[sessionId] = graphic;
-    };
-
-    players.onRemove = (player: any, sessionId: any) => {
-      console.log('Colyseus player removed', player);
-      pixiApp?.stage.removeChild(playerGraphics[sessionId]);
-      delete playerGraphics[sessionId];
-    };
-  }, [players, pixiApp, centerCamera]);
-
-  React.useEffect(() => {
-    players.forEach((player: any, sessionId: any) => {
-      const graphic = playerGraphics[sessionId];
 
       player.onChange = () => {
         graphic.x = player.x;
         graphic.y = player.y;
 
-        console.log('player changed', player);
+        console.log('Player changed', player);
 
         if (sessionId === colyseusRoom.sessionId) {
           centerCamera();
         }
       };
-    });
-  }, [players, pixiApp, centerCamera]);
+    };
+
+    colyseusRoom.state.players.onRemove = (player: any, sessionId: any) => {
+      console.log('Colyseus player removed', player);
+      pixiApp?.stage.removeChild(playerGraphics[sessionId]);
+      delete playerGraphics[sessionId];
+    };
+  }, [colyseusRoom, pixiApp, centerCamera]);
 
   React.useEffect(() => {
     wrapperRef.current?.appendChild(pixiApp.view);
