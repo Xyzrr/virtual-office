57f8093bf27c5a8e7357211a09351689ee40ae55
John Qian <johnlongqian@gmail.com>
2021-02-06 15:37:23 -0800

    scale video to fill panel

--- src/components/RemoteUserPanel.styles.ts
+++ src/components/RemoteUserPanel.styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<{ scale: number }>`
   video {
     display: block;
+    width: 100%;
+    height: 100%;
   }
 `;
