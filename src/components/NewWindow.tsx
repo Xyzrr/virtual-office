5febda24688f73b58f560d4743a102eb30f1fb4b
John Qian <johnlongqian@gmail.com>
2021-02-18 13:35:41 -0800

    change name to Harbor

--- src/components/NewWindow.tsx
+++ src/components/NewWindow.tsx
@@ -42,9 +42,6 @@ const NewWindow: React.FC<NewWindowProps> = ({
   const containerEl = React.useMemo(() => document.createElement('div'), [
     open,
   ]);
-  const containerEl2 = React.useMemo(() => document.createElement('div'), [
-    open,
-  ]);
   const newWindow = React.useRef<Window | null>(null);
 
   React.useEffect(() => {
