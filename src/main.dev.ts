0feaf6dfaf2f11f96a5e393274c20e9dbb6816da
John Qian <johnlongqian@gmail.com>
2021-04-08 16:57:35 -0700

    remove redundant log transports thing

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -31,8 +31,6 @@ import { LIGHT_BACKGROUND } from './components/constants';
 import { openSystemPreferences } from 'electron-util';
 import { autoUpdater } from 'electron-updater';
 
-log.transports.file.level = 'info';
-
 export default class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
