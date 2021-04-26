2078e4f533f06a3ed764a9059f7a7b15c401019c
John Qian <johnlongqian@gmail.com>
2021-04-25 17:37:30 -0700

    beginnings of chatbox

---
+++ src/Chatbox.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  position: absolute;
+  left: 16px;
+  bottom: 16px;
+  width: 300px;
+`;
+
+export const ChatInput = styled.div`
+  background: #444;
+  padding: 8px 12px;
+  border-radius: 4px;
+  color: #aaa;
+`;
