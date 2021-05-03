1a18dc17e8d7e1a302503efb9927102162ec06e4
John Qian <johnlongqian@gmail.com>
2021-05-02 19:11:56 -0700

    fake frame border radius in big sur

--- src/components/FakeMacOSFrame.styles.ts
+++ src/components/FakeMacOSFrame.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<{ bigSur?: boolean }>`
   position: fixed;
   width: 100%;
   height: 100%;
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
   left: 0;
   box-shadow: inset 0 1px 0 0 rgba(255, 255, 255, 0.1),
     inset 0 0 0 1px rgba(255, 255, 255, 0.2);
-  border-radius: 6px;
+  border-radius: ${(props) => (props.bigSur ? 12 : 6)}px;
   z-index: 100;
   pointer-events: none;
 `;
--- src/components/FakeMacOSFrame.tsx
+++ src/components/FakeMacOSFrame.tsx
@@ -1,16 +1,23 @@
 import * as S from './FakeMacOSFrame.styles';
 import React from 'react';
+import os from 'os';
 
 export interface FakeMacOSFrameProps {
   className?: string;
 }
 
 const FakeMacOSFrame: React.FC<FakeMacOSFrameProps> = ({ className }) => {
+  console.log('PROCESS VER', process.version);
   if (process.platform !== 'darwin') {
     return null;
   }
 
-  return <S.Wrapper className={className}></S.Wrapper>;
+  return (
+    <S.Wrapper
+      className={className}
+      bigSur={os.version().startsWith('20.')}
+    ></S.Wrapper>
+  );
 };
 
 export default FakeMacOSFrame;
